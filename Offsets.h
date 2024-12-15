086ae5ad0ed7b620ca34b9319a6c605c5ad381e6
Sunny Servoce <sunnypayment97@gmail.com>
2024-12-15 16:51:53 +0000

    Offsets.h Added
    Offsets.h Added

---
+++ Offsets.h
@@ -0,0 +1,42 @@
+// Those Are Only The most needed Offsets its not ever offsets so dont hate becuse you find nothing like something for magic bullet or something like this 
+// For any Questions Join my Discord https://discord.gg/MJeY3BhJUf or just send me a DM on This acc "onetab_99694"
+
+
+    inline static uintptr_t gworld = 0x170d9d88;
+    inline static uintptr_t local_pawn = 0x350;
+    inline static uintptr_t player_controller = 0x30;
+    inline static uintptr_t local_players = 0x38;
+    inline static uintptr_t player_state = 0x2c8;
+    inline static uintptr_t game_instance = 0x200;
+    inline static uintptr_t game_state = 0x188;
+    inline static uintptr_t player_array = 0x2c0;
+    inline static uintptr_t root_component = 0x1b0;
+    inline static uintptr_t relative_location = 0x138;
+    inline static uintptr_t pawn_private = 0x320;
+    inline static uintptr_t mesh = 0x328;
+    inline static uintptr_t team_index = 0x1261;
+    inline static uintptr_t b_is_dying = 0x718;
+    inline static uintptr_t bone_array = 0x5a8;
+    inline static uintptr_t component_to_world = 0x1e0;
+    inline static uintptr_t last_submit_time_on_screen = 0x30c;
+    inline static uintptr_t platform = 0x430;
+    inline static uintptr_t p_name_structure = 0xaf8;
+    inline static uintptr_t habenaro_component = 0xa38;
+    inline static uintptr_t ranked_progress = 0xd0;
+    inline static uintptr_t current_weapon = 0xa80;
+    inline static uintptr_t weapon_data = 0x6a0;
+    inline static uintptr_t b_has_thanked_bus_driver = 0x13dd;
+    inline static uintptr_t kill_score = 0x1274;
+    inline static uintptr_t season_level_ui_display = 0x1278;
+    inline static uintptr_t velocity = 0x168;
+    inline static uintptr_t projectile_speed = 0x1A24;
+    inline static uintptr_t projectile_gravity = 0x1DF0;
+    inline static uintptr_t levels = 0x1a0;
+    inline static uintptr_t aactor = 0x1F0;
+    inline static uintptr_t primary_pickup_item_entry = 0x360;
+    inline static uintptr_t tier = 0xa2;
+    inline static uintptr_t b_already_searched = 0xdc2;
+    inline static uintptr_t b_ignore_try_to_fire_slot_cooldown_restriction = 0x15f1;
+    inline static uintptr_t b_ads_while_not_on_ground = 0x5759;
+
+// Maybe you Dont need "inline Static"
